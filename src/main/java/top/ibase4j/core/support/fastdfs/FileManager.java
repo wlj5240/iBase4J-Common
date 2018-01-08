b11d4fc71b94c3c39e9f00282e1ccab036fc1ed7
A♂圣翔 <huajieshen@126.com>
2018-01-08 14:41:06 +0800

    优化

--- src/main/java/top/ibase4j/core/support/fastdfs/FileManager.java
+++ src/main/java/top/ibase4j/core/support/fastdfs/FileManager.java
@@ -15,6 +15,7 @@ import org.csource.fastdfs.TrackerClient;
 import org.csource.fastdfs.TrackerServer;
 import org.csource.fastdht.FastDHTClient;
 import org.csource.fastdht.KeyInfo;
+import org.springframework.core.io.DefaultResourceLoader;
 
 import com.alibaba.fastjson.JSON;
 
@@ -27,99 +28,100 @@ import top.ibase4j.core.util.PropertiesUtil;
 @SuppressWarnings("serial")
 public class FileManager implements Config {
     private static Logger logger = LogManager.getLogger();
-	private TrackerServer trackerServer;
-	private StorageServer storageServer;
-	private StorageClient storageClient;
-	private FastDHTClient fastDHTClient;
+    private TrackerServer trackerServer;
+    private StorageServer storageServer;
+    private StorageClient storageClient;
+    private FastDHTClient fastDHTClient;
 
-	private ExecutorService searchService = Executors.newSingleThreadExecutor();
+    private ExecutorService searchService = Executors.newSingleThreadExecutor();
 
-	static { // Initialize Fast DFS Client configurations
-		try {
-			String path = FileManager.class.getResource("/").toString().replace("file:/", "");
-			ClientGlobal.init(path + "fdfs_client.conf");
-			org.csource.fastdht.ClientGlobal.init(path + "fdht_client.conf");
-		} catch (Exception e) {
-			logger.error("", e);
-		}
-	}
+    static { // Initialize Fast DFS Client configurations
+        try {
+            String path = new DefaultResourceLoader().getResource("classpath:fdfs_client.conf").getFile()
+                .getParentFile().getAbsolutePath();
+            ClientGlobal.init(path + "\\fdfs_client.conf");
+            org.csource.fastdht.ClientGlobal.init(path + "\\fdht_client.conf");
+        } catch (Exception e) {
+            logger.error("", e);
+        }
+    }
 
-	public static FileManager getInstance() {
-		return new FileManager();
-	}
+    public static FileManager getInstance() {
+        return new FileManager();
+    }
 
-	private FileManager() {
-		try {
-			TrackerClient trackerClient = new TrackerClient();
-			this.trackerServer = trackerClient.getConnection();
-			this.storageClient = new StorageClient(trackerServer, storageServer);
-			fastDHTClient = new FastDHTClient(true);
-		} catch (Exception ex) {
-			logger.error("", ex);
-		}
-	}
+    private FileManager() {
+        try {
+            TrackerClient trackerClient = new TrackerClient();
+            this.trackerServer = trackerClient.getConnection();
+            this.storageClient = new StorageClient(trackerServer, storageServer);
+            fastDHTClient = new FastDHTClient(true);
+        } catch (Exception ex) {
+            logger.error("", ex);
+        }
+    }
 
-	public void upload(final FileModel file) {
-		logger.info("File Name: " + file.getFilename() + ". File Length: " + file.getContent().length);
+    public void upload(final FileModel file) {
+        logger.info("File Name: " + file.getFilename() + ". File Length: " + file.getContent().length);
 
-		final NameValuePair[] meta_list = new NameValuePair[] { new NameValuePair("mime", file.getMime()),
-				new NameValuePair("size", file.getSize()), new NameValuePair("filename", file.getFilename()) };
+        final NameValuePair[] meta_list = new NameValuePair[]{new NameValuePair("mime", file.getMime()),
+            new NameValuePair("size", file.getSize()), new NameValuePair("filename", file.getFilename())};
 
-		long startTime = System.currentTimeMillis();
-		String[] uploadResults = null;
-		try {
-			uploadResults = storageClient.upload_file(file.getContent(), file.getExt(), meta_list);
-		} catch (IOException e) {
-			logger.error("IO Exception when uploadind the file: " + file.getFilename(), e);
-		} catch (Exception e) {
-			logger.error("Non IO Exception when uploadind the file: " + file.getFilename(), e);
-		}
+        long startTime = System.currentTimeMillis();
+        String[] uploadResults = null;
+        try {
+            uploadResults = storageClient.upload_file(file.getContent(), file.getExt(), meta_list);
+        } catch (IOException e) {
+            logger.error("IO Exception when uploadind the file: " + file.getFilename(), e);
+        } catch (Exception e) {
+            logger.error("Non IO Exception when uploadind the file: " + file.getFilename(), e);
+        }
 
-		if (uploadResults == null) {
-			logger.error("upload file fail, error code: " + storageClient.getErrorCode());
-		} else {
+        if (uploadResults == null) {
+            logger.error("upload file fail, error code: " + storageClient.getErrorCode());
+        } else {
 
-			final String groupName = uploadResults[0];
-			final String remoteFileName = uploadResults[1];
+            final String groupName = uploadResults[0];
+            final String remoteFileName = uploadResults[1];
 
-			String fileAbsolutePath = PropertiesUtil.getString("remote.file.uri.prefix") + groupName + SEPARATOR
-					+ remoteFileName;
-			file.setRemotePath(fileAbsolutePath);
-			logger.info("upload_file time used: " + (System.currentTimeMillis() - startTime) + " ms. group_name: "
-					+ groupName + ", remoteFileName:" + " " + remoteFileName);
+            String fileAbsolutePath = PropertiesUtil.getString("remote.file.uri.prefix") + groupName + SEPARATOR
+                + remoteFileName;
+            file.setRemotePath(fileAbsolutePath);
+            logger.info("upload_file time used: " + (System.currentTimeMillis() - startTime) + " ms. group_name: "
+                + groupName + ", remoteFileName:" + " " + remoteFileName);
 
-			searchService.execute(new Runnable() {
-				public void run() {
-					try {
-						KeyInfo keyInfo = new KeyInfo(file.getNamespace(), file.getObjectId(), file.getKey());
-						FastDfsFile fastDfsFile = new FastDfsFile();
-						fastDfsFile.setGroupName(groupName);
-						fastDfsFile.setFileName(remoteFileName);
-						fastDfsFile.setNameValuePairs(meta_list);
-						fastDHTClient.set(keyInfo, JSON.toJSONString(fastDfsFile));
-					} catch (Exception e) {
-						logger.error("", e);
-					}
-				}
-			});
-		}
-	}
+            searchService.execute(new Runnable() {
+                public void run() {
+                    try {
+                        KeyInfo keyInfo = new KeyInfo(file.getNamespace(), file.getObjectId(), file.getKey());
+                        FastDfsFile fastDfsFile = new FastDfsFile();
+                        fastDfsFile.setGroupName(groupName);
+                        fastDfsFile.setFileName(remoteFileName);
+                        fastDfsFile.setNameValuePairs(meta_list);
+                        fastDHTClient.set(keyInfo, JSON.toJSONString(fastDfsFile));
+                    } catch (Exception e) {
+                        logger.error("", e);
+                    }
+                }
+            });
+        }
+    }
 
-	public FileInfo getFile(String namespace, String objectId, String key) {
-		try {
-			KeyInfo keyInfo = new KeyInfo(namespace, objectId, key);
-			String info = fastDHTClient.get(keyInfo);
-			FastDfsFile fastDfsFile = JSON.parseObject(info, FastDfsFile.class);
-			return storageClient.get_file_info(fastDfsFile.getGroupName(), fastDfsFile.getFileName());
-		} catch (IOException e) {
-			logger.error("IO Exception: Get File from Fast DFS failed", e);
-		} catch (Exception e) {
-			logger.error("Non IO Exception: Get File from Fast DFS failed", e);
-		}
-		return null;
-	}
+    public FileInfo getFile(String namespace, String objectId, String key) {
+        try {
+            KeyInfo keyInfo = new KeyInfo(namespace, objectId, key);
+            String info = fastDHTClient.get(keyInfo);
+            FastDfsFile fastDfsFile = JSON.parseObject(info, FastDfsFile.class);
+            return storageClient.get_file_info(fastDfsFile.getGroupName(), fastDfsFile.getFileName());
+        } catch (IOException e) {
+            logger.error("IO Exception: Get File from Fast DFS failed", e);
+        } catch (Exception e) {
+            logger.error("Non IO Exception: Get File from Fast DFS failed", e);
+        }
+        return null;
+    }
 
-	public void deleteFile(String groupName, String remoteFileName) throws Exception {
-		storageClient.delete_file(groupName, remoteFileName);
-	}
+    public void deleteFile(String groupName, String remoteFileName) throws Exception {
+        storageClient.delete_file(groupName, remoteFileName);
+    }
 }
